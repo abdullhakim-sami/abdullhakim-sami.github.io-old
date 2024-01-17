d89e9c901389d270a5cf48bc39d4cd8660545868
abdullhakim-sami <kyonasdev@gmail.com>
2024-01-17 22:24:23 +0300

    idk

--- src/Pages/Home_.elm
+++ src/Pages/Home_.elm
@@ -3,9 +3,10 @@ module Pages.Home_ exposing (Model, Msg, page)
 -- import Debug exposing (log)
 
 import Effect exposing (Effect)
-import Element exposing (Element, alignTop, centerX, el, fill, height, padding, paddingEach, rgb255, text, width)
+import Element exposing (alignTop, centerX, el, fill, height, padding, paddingEach, rgb255, text, width)
 import Element.Background exposing (color)
 import Element.Border
+import Element.Font
 import Element.Input exposing (button)
 import Page exposing (Page)
 import Pages.NotFound_ exposing (Msg)
@@ -88,11 +89,19 @@ view model =
     { title = "Homepage"
     , attributes = []
     , element =
-        el [ color (rgb255 242 199 80), width fill, height fill ]
-            (el [ Element.alignTop, centerX, padding 10 ]
-                (button [ paddingEach { top = 10, bottom = 10, left = 3, right = 3 }, Element.Border.solid, Element.Border.width 5, Element.Border.rounded 20, Element.Border.color (rgb255 142 199 80) ]
+        el [ color (rgb255 21 64 38), width fill, height fill ]
+            (el [ alignTop, centerX, padding 10 ]
+                (button
+                    [ color (rgb255 21 64 78)
+                    , paddingEach { top = 10, bottom = 10, left = 6, right = 6 }
+                    , Element.Border.solid
+                    , Element.Border.width 2
+                    , Element.Border.rounded 20
+                    , Element.Border.color (rgb255 142 199 80)
+                    , Element.Font.color (rgb255 142 199 80)
+                    ]
                     { onPress = Just ButtonPressed
-                    , label = text "Content Management System"
+                    , label = text "Elm CMS"
                     }
                 )
             )
